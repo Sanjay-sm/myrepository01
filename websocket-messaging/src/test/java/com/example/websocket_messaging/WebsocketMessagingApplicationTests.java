777ac948326c57a20e6ca4568fdf54ac546275bb
Sanjay S <163410347+Sanjay-sm@users.noreply.github.com>
2024-07-08 14:55:08 +0530

    Add files via upload

---
+++ websocket-messaging/src/test/java/com/example/websocket_messaging/WebsocketMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
